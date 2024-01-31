fdd788ef2da70dab5746007ecabf0bb218dba3b9
Brianna Tran <112038594+briannatran1@users.noreply.github.com>
2024-01-31 14:07:34 -0800

    Update sort_array.py

--- sort_array.py
+++ sort_array.py
@@ -53,3 +53,32 @@ class Solution:
 # iterate through nums using nested loops
 # if curr is greater than next,
 # swap elems
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        '''requirements:
+            - fn takes in list of ints
+            - do not use built-in fns
+            - O(n log(n)) time 
+            - return arr in ascending order
+        '''
+        n = len(nums)
+        for i in range(n):
+            min_index = i
+            for j in range(i + 1, n):
+                if nums[j] < nums[min_index]:
+                    min_index = j
+            nums[i], nums[min_index] = nums[min_index], nums[i]
+        
+        return nums
+
+# selection sort:
+# find min elem and swap w unsorted elem
+
+# iterate through nums using nested loops
+# outer loop iterates through each element in the list
+# find index of min val
+# inner loop starts from the next element after the current one (i + 1) and goes until the end of the list.
+# if first elem is greater than min, update min index to smaller one
+# swap min w current elem
+# return list
