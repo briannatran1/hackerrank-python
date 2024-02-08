2f34b3660e8bdcd326a18f4571958e15b656d5e2
Brianna Tran <112038594+briannatran1@users.noreply.github.com>
2024-02-08 15:04:32 -0800

    Create find_peak_elem.py

---
+++ find_peak_elem.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            # elem at mid is less than left neighbor, search left side
+            if mid > 0 and nums[mid] < nums[mid - 1]:
+                right = mid - 1
+            # elem at mid is less than right neighbor, search right side
+            elif mid < right and nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                return mid
