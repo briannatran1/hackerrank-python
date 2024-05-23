e500e3818f50323f8204c3f00826bc7af28e8150
Brianna Tran <112038594+briannatran1@users.noreply.github.com>
2024-05-22 21:11:12 -0700

    Update max_depth.py

--- max_depth.py
+++ max_depth.py
@@ -23,3 +23,45 @@ class Solution:
         left_depth = self.maxDepth(root.left)
         right_depth = self.maxDepth(root.right)
         return max(left_depth, right_depth) + 1
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        '''req:
+            - fn takes in the root of a binary tree
+            - return its max depth: num of nodes along longest path starting at root node
+
+            >>> root = [3,9,20,null,null,15,7]
+            3
+
+            iterative approach
+        '''
+        # base case: root is null
+        # initialize a stack containing root node and its depth
+        # initialize a res var --> keeps track of max depth
+        # loop until stack is empty
+        # pop node and its depth from stack
+        # if node is not null,
+        # update res to max value of either curr res or curr depth
+        # append left child and right child of curr node to stack along w/ its depth
+        # return res
+        if not root: 
+            return 0
+
+        stack = [[root, 1]]
+        res = 1
+        
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        
+        return res
