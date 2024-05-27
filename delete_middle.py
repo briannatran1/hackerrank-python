910074ce3a56dcebaf4f1edc5c224d8f812f4a46
Brianna Tran <112038594+briannatran1@users.noreply.github.com>
2024-05-26 22:53:06 -0700

    Create delete_middle.py

---
+++ delete_middle.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        '''req:
+            - fn takes in head of LL
+            - delete middle node and return LL
+            - middle node: size of LL // 2
+
+            >>> [1,2,3,4]
+            1 -> 2 -> 4
+
+            >>> head = [1,3,4,7,1,2,6]
+            1 -> 3 -> 4 -> 1 -> 2 -> 6
+        '''
+        # base case: head is null --> nothing to delete
+        # create dummy node prev
+        # set prev.next to head 
+        # initialize fast and slow pointers
+        # traverse LL using 2 pointer technique
+        if not head:
+            return None
+        
+        prev = ListNode(0)
+        prev.next = head
+        slow = prev
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # skips over middle node and points to node after middle node
+        slow.next = slow.next.next 
+        # return list; prev is dummy and prev.next is start of LL
+        return prev.next
