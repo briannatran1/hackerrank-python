707a1b71b3a488444f248780255f2edb213469ae
Brianna Tran <112038594+briannatran1@users.noreply.github.com>
2024-05-28 16:25:17 -0700

    Update is_anagram.py

--- is_anagram.py
+++ is_anagram.py
@@ -24,3 +24,19 @@ class Solution:
 # iterate through s and increment count for each char
 # iterate through t and decrement count for each char
 # check if any char has non-zero freq --> not anagrams
+
+# 2nd sol
+import collections
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        '''req:
+            - fn takes in 2 strs
+            - return True if they're anagrams
+        '''
+        # create Counter obj for each str
+        # compare counter objs and return True if they are the same
+        counter_s = Counter(s)
+        counter_t = Counter(t)
+
+        return counter_s == counter_t
