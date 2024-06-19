71ac13e0d883ec9d14b1aa0208ab3f2c8d9f64d3
Brianna Tran <112038594+briannatran1@users.noreply.github.com>
2024-06-18 21:55:33 -0700

    Update good_nodes.py

--- good_nodes.py
+++ good_nodes.py
@@ -30,3 +30,42 @@ class Solution:
         
         dfs(root)
         return self.good_nodes
+
+
+# stack solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        '''req:
+            - fn given 
+        '''
+        # init stack to tuple root node and root.val
+        # init count var to 0
+        # loop while stack has elems in it,
+        # pop tuple and assign to node and max_num
+        # if node.val >= max_num, increment count
+        # set max_num to the max of the 2
+        # if node.left is not null, append node.left and max_num to stack
+        # do the same with node.right
+        # return count
+        stack = [(root, root.val)]
+        count = 0
+
+        while stack:
+            node, max_num = stack.pop() #
+            if node.val >= max_num:
+                count += 1
+            max_num = max(max_num, node.val)
+
+            if node.left:
+                stack.append((node.left, max_num))
+            if node.right:
+                stack.append((node.right, max_num))
+        
+        return count
